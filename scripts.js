61541e34623cf9757c5bf935ebd1d1a52bc8645c
Jason <jwrounds@usfca.edu>
2021-03-23 15:34:19 -0500

    Implemented abort mission event handler

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
     const flightStatus = document.querySelector("#flightStatus");
     const shuttleBackground = document.querySelector("#shuttleBackground");
     const shuttleHeight = document.querySelector("#spaceShuttleHeight");
@@ -13,20 +13,16 @@ window.addEventListener("load", function(){
     const right = document.querySelector("#right");
     const left = document.querySelector("#left");
 
-    let posTop = shuttle.style.top;
-    let posBottom = shuttle.style.bottom;
-    let posLeft = shuttle.style.left;
-    let postRight = shuttle.style.right;
+    let shuttlePixelHeight = Number(shuttle.style.top.slice(0, shuttle.style.top.length - 2));
+    let shuttlePixelLeft = Number(shuttle.style.left.slice(0, shuttle.style.left.length - 2));
+    let shuttlePixelRight = Number(shuttle.style.right.slice(0, shuttle.style.right.length - 2));
 
-    takeOffButton.addEventListener('click', function(){
+    takeOffButton.addEventListener('click', function() {
         let confirmation = window.confirm("Confirm that theshuttle is ready for takeoff.");
         if (confirmation) {
             flightStatus.textContent = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
-
-            let shuttlePixelHeight = Number(posTop.slice(0, posTop.length - 2)) - 50;
-            shuttle.style.top = shuttlePixelHeight + 'px';
-            
+            shuttle.style.top = shuttlePixelHeight - 50 + 'px';
             let height = Number(shuttleHeight.textContent);
             shuttleHeight.textContent = height += 10000;
         }
@@ -37,5 +33,17 @@ window.addEventListener("load", function(){
         flightStatus.textContent = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
         shuttleHeight.textContent = 0;
+        shuttle.style.top = "250px";
     });
+
+    abortButton.addEventListener('click', function() {
+       let confirmation = confirm( "Confirm that you want to abort the mission." );
+       if (confirmation) {
+           flightStatus.textContent = "Flight aborted";
+           shuttleBackground.style.backgroundColor = "green";
+           shuttleHeight.textContent = 0;
+           shuttle.style.top = "250px";
+       }
+    });
+
 });
